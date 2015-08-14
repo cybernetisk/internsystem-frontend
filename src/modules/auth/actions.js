3aa1efb3506dc4dd9cece0355c57af05d46e7770
Henrik Steen <henrist@henrist.net>
2015-08-14 23:14:36 +0200

    Remove some old logging

--- src/modules/auth/actions.js
+++ src/modules/auth/actions.js
@@ -12,7 +12,6 @@ export default {
     reactor.dispatch(RECEIVE_AUTHDATA_START)
 
     AuthService.getAuthData().then((data) => {
-      console.log('got data', data)
       reactor.dispatch(RECEIVE_AUTHDATA_SUCCESS, data)
     }).catch((err) => {
       reactor.dispatch(RECEIVE_AUTHDATA_FAILURE, err)
--- src/modules/auth/getters.js
+++ src/modules/auth/getters.js
@@ -3,7 +3,6 @@ const authdata = ['authdata']
 const isLoggedIn = [
   ['authdata'],
   (authdata) => {
-    console.log('isloggedin')
     return authdata.get('data') && authdata.get('data').get('loggedIn')
   }
 ]
@@ -11,7 +10,6 @@ const isLoggedIn = [
 const userDetails = [
   ['authdata'],
   (authdata) => {
-    console.log('authdata', authdata)
     return authdata.get('data') && authdata.get('data').get('details') ? authdata.get('data').get('details').toJS() : null
   }
 ]
