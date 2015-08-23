cc0479e59defcfe3f80db1555f844a6fc829a09e
Henrik Steen <henrist@henrist.net>
2015-08-23 18:16:10 +0200

    React component for pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default class extends React.Component {
+
+  static propTypes = {
+    active: React.PropTypes.number.isRequired,
+    pages: React.PropTypes.number.isRequired,
+    onChange: React.PropTypes.func.isRequired
+  }
+
+  handleChange(event, newValue) {
+    event.preventDefault()
+
+    if (newValue != this.props.active && newValue >= 1 && newValue <= this.props.pages) {
+      this.props.onChange(newValue)
+    }
+  }
+
+  render() {
+    return (
+      <div className="text-center">
+        <ul className="pagination">
+          <li><a href onClick={event => this.handleChange(event, 1)}>&laquo;</a></li>
+          <li><a href onClick={event => this.handleChange(event, this.props.active-1)}>&lsaquo;</a></li>
+          <li><a>{this.props.active}/{this.props.pages}</a></li>
+          <li><a href onClick={event => this.handleChange(event, this.props.active+1)}>&rsaquo;</a></li>
+          <li><a href onClick={event => this.handleChange(event, this.props.pages)}>&raquo;</a></li>
+        </ul>
+      </div>
+    )
+  }
+}
