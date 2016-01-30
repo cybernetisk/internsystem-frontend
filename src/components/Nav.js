7293baf60082110ae4332fac150ac6d9ba62b9f6
Henrik Steen <henrist@henrist.net>
2016-01-30 18:55:02 +0100

    Use react-bootstrap for navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router'
 import { connect } from 'nuclear-js-react-addons'
+import {Navbar, Nav as RefNav, NavItem, MenuItem} from 'react-bootstrap'
 
 import { authdata, userDetails, isLoggedIn } from '../modules/auth/getters'
 import NavDropdown from './NavDropdown'
@@ -15,34 +16,30 @@ export default class Nav extends React.Component {
   renderProfileMenu() {
     if (this.props.authdata.get('isLoading')) {
       return (
-        <ul className='nav navbar-nav navbar-right'>
-          <p className='navbar-text'>Loading data ..</p>
-        </ul>
+        <RefNav pullRight>
+          <li className='navbar-text'>Loading data ..</li>
+        </RefNav>
       )
     } else if (this.props.authdata.get('error')) {
       return (
-        <ul className='nav navbar-nav navbar-right'>
-          <p className='navbar-text'>Error: {this.props.authdata.get('error')}</p>
-        </ul>
+        <RefNav pullRight>
+          <li className='navbar-text'>Error: {this.props.authdata.get('error')}</li>
+        </RefNav>
       )
     } else if (this.props.isLoggedIn) {
       return (
-        <ul className='nav navbar-nav navbar-right'>
-          <NavDropdown>
-            <a className='dropdown-toggle' data-toggle='dropdown' role='button'
-              aria-expanded='false'>{this.props.userDetails.realname} <span className='caret'></span></a>
-            <ul className='dropdown-menu' role='menu'>
-              <NavLink to='auth.profile'>Profile</NavLink>
-              <NavLink to='auth.logout'>Log out</NavLink>
-            </ul>
+        <RefNav pullRight>
+          <NavDropdown title={this.props.userDetails.realname} id="nav-auth-dropdown">
+            <NavLink to='auth.profile'>Profile</NavLink>
+            <NavLink to='auth.logout'>Log out</NavLink>
           </NavDropdown>
-        </ul>
+        </RefNav>
       )
     } else {
       return (
-        <ul className='nav navbar-nav navbar-right'>
+        <RefNav pullRight>
           <NavLink to='auth.login'>Log in with UiO-account</NavLink>
-        </ul>
+        </RefNav>
       )
     }
   }
@@ -51,85 +48,53 @@ export default class Nav extends React.Component {
     let profileMenu = this.renderProfileMenu()
 
     return (
-      <nav className='navbar navbar-inverse navbar-fixed-top' role='navigation'>
-        <div className='container'>
-          <div className='navbar-header'>
-            <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar'
-              aria-expanded='false' aria-controls='navbar'>
-              <span className='sr-only'>Toggle navigation</span>
-              <span className='icon-bar'></span>
-              <span className='icon-bar'></span>
-              <span className='icon-bar'></span>
-            </button>
-            <Link className='navbar-brand' to='index'>CYB internsystem</Link>
-          </div>
-          <div id='navbar' className='collapse navbar-collapse'>
-            <ul className='nav navbar-nav'>
-              <NavDropdown>
-                <a className='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>
-                  Products
-                  <span className='caret'></span>
-                </a>
-                <ul className='dropdown-menu' role='menu'>
-                  <NavLink to='varer/accounts'>Account list</NavLink>
-                  <NavLink to='varer/inventorycounts'>Inventory counts</NavLink>
-                  <NavLink to='varer/inventory'>Inventory items</NavLink>
-                  <NavLink to='varer/salesestimates'>Sales estimates</NavLink>
-                  <NavLink to='varer/products'>Sales products</NavLink>
-                  <NavLink to='varer/vendors'>Vendor list</NavLink>
-                </ul>
-              </NavDropdown>
-              <NavDropdown>
-                <a className='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>
-                  Z
-                  <span className='caret'></span>
-                </a>
-                <ul className='dropdown-menu' role='menu'>
-                  <NavLink to='z'>Overview</NavLink>
-                  <li className='divider'></li>
-                  <li className='dropdown-header'>Modules</li>
-                  <NavLink to='z/stats'>Statistics</NavLink>
-                </ul>
-              </NavDropdown>
-              <NavDropdown>
-                <a className='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>
-                  Calendar
-                  <span className='caret'></span>
-                </a>
-                <ul className='dropdown-menu' role='menu'>
-                  <NavLink to='cal/list'>Overview</NavLink>
-                </ul>
-              </NavDropdown>
-              <NavDropdown>
-                <a className='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>
-                  Vouchers
-                  <span className='caret'></span>
-                </a>
-                <ul className='dropdown-menu' role='menu'>
-                  <NavLink to='voucher/stats'>Overview</NavLink>
-                  <NavLink to='voucher/uselogs'>Use logs</NavLink>
-                  <NavLink to='voucher/worklogs'>Work logs</NavLink>
-                </ul>
-              </NavDropdown>
-              <NavDropdown>
-                <a className='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>
-                  Other
-                  <span className='caret'></span>
-                </a>
-                <ul className='dropdown-menu' role='menu'>
-                  <li><a target='_self' href='http://heim.ifi.uio.no/cyb/tilganger/'>Access control management (Garm)</a></li>
-                  <li><a target='_self' href='https://jira.cyb.no/'>JIRA (task management)</a></li>
-                  <li><a target='_self' href='https://confluence.cyb.no/'>Wiki (Confluence)</a></li>
-                  <li><a target='_self' href='https://github.com/cybrairai/'>GitHub-organization</a></li>
-                  <li><a target='_self' href='https://cybernetisk.slack.com/'>Slack</a></li>
-                  <li><a target='_self' href='https://www.facebook.com/groups/CYB.intern/'>Facebook group: CYB Intern</a></li>
-                </ul>
-              </NavDropdown>
-            </ul>
-            {profileMenu}
-          </div>
-        </div>
-      </nav>
+      <Navbar fixedTop inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="index">CYB internsystem</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <RefNav>
+            <NavDropdown title="Products" id="nav-products-dropdown">
+              <NavLink to='varer/accounts'>Account list</NavLink>
+              <NavLink to='varer/inventorycounts'>Inventory counts</NavLink>
+              <NavLink to='varer/inventory'>Inventory items</NavLink>
+              <NavLink to='varer/salesestimates'>Sales estimates</NavLink>
+              <NavLink to='varer/products'>Sales products</NavLink>
+              <NavLink to='varer/vendors'>Vendor list</NavLink>
+            </NavDropdown>
+            <NavDropdown title="Z" id="nav-z-dropdown">
+              <NavLink to='z'>Overview</NavLink>
+              <MenuItem divider/>
+              <MenuItem header>Modules</MenuItem>
+              <NavLink to='z/stats'>Statistics</NavLink>
+            </NavDropdown>
+            <NavDropdown title="Calendar" id="nav-cal-dropdown">
+              <NavLink to='cal/list'>Overview</NavLink>
+            </NavDropdown>
+            <NavDropdown title="Vouchers" id="nav-voucher-dropdown">
+              <NavLink to='voucher/stats'>Overview</NavLink>
+              <NavLink to='voucher/uselogs'>Use logs</NavLink>
+              <NavLink to='voucher/worklogs'>Work logs</NavLink>
+            </NavDropdown>
+            <NavDropdown title="Other" id="nav-other-dropdown">
+              <li>
+                <a target="_self" href='http://heim.ifi.uio.no/cyb/tilganger/'>Access control management (Garm)</a>
+              </li>
+              <li><a target='_self' href='https://jira.cyb.no/'>JIRA (task management)</a></li>
+              <li><a target='_self' href='https://confluence.cyb.no/'>Wiki (Confluence)</a></li>
+              <li><a target='_self' href='https://github.com/cybrairai/'>GitHub-organization</a></li>
+              <li><a target='_self' href='https://cybernetisk.slack.com/'>Slack</a></li>
+              <li>
+                <a target='_self' href='https://www.facebook.com/groups/CYB.intern/'>Facebook group: CYB Intern</a>
+              </li>
+            </NavDropdown>
+          </RefNav>
+          {profileMenu}
+        </Navbar.Collapse>
+      </Navbar>
     )
   }
 }
--- src/components/NavDropdown.js
+++ src/components/NavDropdown.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import {NavDropdown as RefNavDropdown} from 'react-bootstrap'
+
 export default class NavDropdown extends React.Component {
+
+  static propTypes = RefNavDropdown.propTypes
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isActive: false
+    }
+  }
 
   componentDidUpdate() {
     // this is really a hack to set active-class if a child is active
     // should probably not use DOM-operations like this...
     let dom = ReactDOM.findDOMNode(this)
-    dom.className = dom.querySelector('.active') !== null
-      ? 'dropdown active'
-      : 'dropdown'
+
+    let isActive = dom.querySelector('.active') !== null
+    if (isActive != this.state.isActive) {
+      this.setState({isActive})
+    }
   }
 
   render() {
-    return <li className='dropdown'>{this.props.children}</li>
+    let className = this.state.isActive ? 'active' : ''
+
+    return (
+      <RefNavDropdown className={className} {...this.props}>
+        {this.props.children}
+      </RefNavDropdown>
+    )
   }
 }
