c7ddbf918683676514782d2a3e5bc4117f089ac4
Henrik Steen <henrist@henrist.net>
2016-02-17 16:50:08 +0100

    Fix wrong variable references

--- src/modules/varer/inventoryItems/List.js
+++ src/modules/varer/inventoryItems/List.js
@@ -78,7 +78,7 @@ export default class List extends React.Component {
     }
 
     if (newProps.query.page != this.props.query.page) {
-      fetchIventoryItems(newProps.query.page || 1)
+      fetchInventoryItems(newProps.query.page || 1)
     }
   }
 
@@ -111,7 +111,7 @@ export default class List extends React.Component {
   }
 
   handlePageChange(newPage) {
-    fetchIventoryItems(newPage)
+    fetchInventoryItems(newPage)
 
     let page = newPage !== 1 ? newPage : undefined
     this.updateQuery('page', page)
