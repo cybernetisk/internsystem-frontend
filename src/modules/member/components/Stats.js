9d5b0282b4a25cc069a6b4d4f415bc0ff2aba016
Mats Schjølberg <mats@schjolberg.net>
2016-03-17 16:50:39 +0100

    Added summery of members over several semesters.

--- src/modules/member/components/Stats.js
+++ src/modules/member/components/Stats.js
@@ -22,26 +22,39 @@ export default class Stats extends React.Component {
     if (!this.props.stats.get('data')) {
       return
     }
+    var normal = 0
+    var lifetime = 0
+    var honorary = 0
 
     return (
       <table className="table-responsive table">
         <thead>
-          <tr>
-            <th>Semester</th>
-            <th>Semester members</th>
-            <th>Lifetime members</th>
-            <th>Honorary members</th>
-          </tr>
+        <tr>
+          <th>Semester</th>
+          <th>Semester members</th>
+          <th>Lifetime members</th>
+          <th>Honorary members</th>
+        </tr>
         </thead>
         <tbody>
-          {this.props.stats.get('data').toList().toJS().map((stats) => (
-            <tr key={stats.id}>
-              <td><Link to={`/member/semester/${stats.id}`}>{stats.semester}</Link></td>
-              <td>{stats.normal}</td>
-              <td>{stats.lifetime}</td>
-              <td>{stats.honorary}</td>
-            </tr>
-          ))}
+        {this.props.stats.get('data').toList().toJS().map((stats) => (
+          <tr key={stats.id}>
+            <td><Link to={`/member/semester/${stats.id}`}>{stats.semester}</Link></td>
+            <td>{stats.normal}</td>
+            <td>{stats.lifetime}</td>
+            <td>{stats.honorary}</td>
+            {normal += stats.normal}
+            {lifetime += stats.lifetime}
+            {honorary += stats.honorary}
+          </tr>
+
+        ))}
+        <tr>
+          <td>All</td>
+          <td>{normal}</td>
+          <td>{lifetime}</td>
+          <td>{honorary}</td>
+        </tr>
         </tbody>
       </table>
     )
