02e43eb1b97fa73153195681d242726432a69d04
Mats Astrup Schjølberg <mats@schjolberg.net>
2020-03-08 01:34:14 +0100

    Fixed white page for lifetime member page by removing uneeded redux and nuclear connect in component

--- src/modules/member/components/Lifetime.js
+++ src/modules/member/components/Lifetime.js
@@ -1,18 +1,7 @@
-import { connect } from "nuclear-js-react-addons-chefsplate"
 import React from "react"
-import { connect as reduxConnect } from "react-redux"
-import { getIsLoggedIn } from "../../auth/selectors"
 import * as actions from "../actions"
-import * as getters from "../getters"
 import List from "./List"
 
-@connect(() => ({
-  isLoggedIn,
-  member: getters.members,
-}))
-@reduxConnect(state => ({
-  isLoggedIn: getIsLoggedIn(state),
-}))
 export default class Lifetime extends React.Component {
   componentDidMount() {
     actions.getLifetimeMember(1, 50)
